53e5f355060346f3b8d332ea821e9fdd73aceafe
mustafameh <786mehmoodmustafa@gmail.com>
2023-06-06 11:15:17 +0530

    comments

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,19 +1,28 @@
-from flask import Flask, request
+# Import the necessary modules
+from flask import Flask, request, render_template
 import os
-from flask import Flask, render_template
 
+# Initialize a Flask application
 app = Flask(__name__)
 
+# Route for the home page
 @app.route('/')
 def home():
+    # Render the home page
     return render_template('index.html')
 
+# Route for handling the recording
 @app.route('/record', methods=['POST'])
 def record():
+    # Retrieve the audio data from the request
     audio_file = request.files['audio_data']
+
+    # Save the audio data as a .webm file
     audio_file.save('audio.webm')
+
+    # Return a success response
     return "success", 200
 
+# Run the Flask application
 if __name__ == "__main__":
     app.run(port=5000)
-
--- WebApp/static/scripts.js
+++ WebApp/static/scripts.js
@@ -1,52 +1,80 @@
+// Declare the variables that will hold the MediaRecorder object and the audio data
 let mediaRecorder;
 let recordedBlobs;
 
+// Attach an event listener to the record button to start recording when it's clicked
 document.getElementById('recordButton').addEventListener('click', startRecording);
 
+// Function to start recording audio
 function startRecording() {
+    // Hide the record button
     document.getElementById('recordButton').style.display = 'none';
+
+    // Initialize the countdown
     let countdownNumber = 3;
     let countdownElement = document.getElementById('countdown');
     countdownElement.textContent = countdownNumber;
 
+    // Start the countdown
     let countdownInterval = setInterval(() => {
         countdownNumber--;
         countdownElement.textContent = countdownNumber;
+
+        // Once the countdown reaches 0, start recording
         if (countdownNumber === 0) {
+            // Stop the countdown
             clearInterval(countdownInterval);
+
+            // Update the UI to show that recording has started
             countdownElement.textContent = 'Your voice is being recorded';
             document.getElementById('recordingIndicator').style.display = 'block';
 
-            // Start recording
+            // Request access to the microphone and start recording
             navigator.mediaDevices.getUserMedia({ audio: true, video: false })
             .then(stream => {
+                // Handle successful stream acquisition
                 handleSuccess(stream);
-                // Move setTimeout to ensure handleSuccess has finished before endRecording is called
-                setTimeout(endRecording, 2000); // 2 seconds recording
+
+                // Stop recording after 2 seconds
+                setTimeout(endRecording, 2000);
             });
         }
     }, 1000);
 }
 
-
+// Function to handle successful stream acquisition
 function handleSuccess(stream) {
+    // Initialize the array that will hold the recorded audio data
     recordedBlobs = [];
+
+    // Create a new MediaRecorder instance
     mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+
+    // Specify what to do when audio data becomes available
     mediaRecorder.ondataavailable = (event) => {
+        // If there's data available, add it to the array
         if (event.data && event.data.size > 0) {
             recordedBlobs.push(event.data);
         }
     };
+
+    // Start recording
     mediaRecorder.start(10); // A timeslice is passed to ensure data is available even for short clips
 }
 
+// Function to stop recording and send the audio data to the server
 function endRecording() {
+    // Stop the MediaRecorder
     mediaRecorder.stop();
 
-    // When creating the Blob, use the same MIME type as the MediaRecorder
+    // Create a Blob from the recorded audio data
     const blob = new Blob(recordedBlobs, {type: 'audio/webm'});
+
+    // Create a FormData object to hold the audio data
     const formData = new FormData();
     formData.append('audio_data', blob);
+
+    // Send the audio data to the server
     fetch('/record', {
         method: 'POST',
         body: formData
@@ -61,3 +89,4 @@ function endRecording() {
         document.getElementById('countdown').textContent = '';
     }, 2000);
 }
+
